625e051ea6665e6eecf80621b309760d3d7a0481
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2025-05-22 15:48:56 +0530

    Update stream.py

--- Oneforall/utils/stream/stream.py
+++ Oneforall/utils/stream/stream.py
@@ -5,15 +5,33 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
+from Oneforall import YouTube, app
+from Oneforall.core.call import Hotty
+
+# Define a dictionary to track the last message timestamp for each user
+user_last_message_time = {}
+user_command_count = {}
+# Define the threshold for command spamming (e.g., 20 commands within 60 seconds)
+SPAM_THRESHOLD = 2
+SPAM_WINDOW_SECONDS = 5
+
+
+from pyrogram.types import InlineKeyboardMarkup
+from youtubesearchpython.__future__ import VideosSearch
+
+import config
 from Oneforall import Carbon, YouTube, app
-from Oneforall.core.call import Hotty as ZYRO
+from Oneforall.core.call import Hotty
 from Oneforall.misc import db
 from Oneforall.utils.database import add_active_video_chat, is_active_chat
 from Oneforall.utils.exceptions import AssistantErr
-from Oneforall.utils.inline import aq_markup, close_markup, stream_markup
-from Oneforall.utils.pastebin import HottyBin as ZYROBin
+from Oneforall.utils.inline import (
+    aq_markup,
+    close_markup,
+    stream_markup,
+    stream_markup2,
+)
 from Oneforall.utils.stream.queue import put_queue, put_queue_index
-from Oneforall.utils.thumbnails import get_thumb
 
 
 async def stream(
@@ -32,7 +50,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await ZYRO.force_stop_stream(chat_id)
+        await Hotty.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -78,8 +96,8 @@ async def stream(
                         vidid, mystic, video=status, videoid=True
                     )
                 except:
-                    raise AssistantErr(_["play_3"])
-                await ZYRO.join_call(
+                    await mystic.edit_text(_["play_3"])
+                await Hotty.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -99,7 +117,7 @@ async def stream(
                     forceplay=forceplay,
                 )
                 img = await get_thumb(vidid)
-                button = stream_markup(_, chat_id)
+                button = stream_markup(_, vidid, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
                     photo=img,
@@ -111,12 +129,13 @@ async def stream(
                     ),
                     reply_markup=InlineKeyboardMarkup(button),
                 )
+
                 db[chat_id][0]["mystic"] = run
                 db[chat_id][0]["markup"] = "stream"
         if count == 0:
             return
         else:
-            link = await ZYROBin(msg)
+            link = await brandedBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -142,7 +161,7 @@ async def stream(
                 vidid, mystic, videoid=True, video=status
             )
         except:
-            raise AssistantErr(_["play_3"])
+            await mystic.edit_text(_["play_3"])
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -155,17 +174,21 @@ async def stream(
                 user_id,
                 "video" if video else "audio",
             )
+            img = await get_thumb(vidid)
             position = len(db.get(chat_id)) - 1
             button = aq_markup(_, chat_id)
-            await app.send_message(
+            await app.send_photo(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
+                photo=img,
+                caption=_["queue_4"].format(
+                    position, title[:18], duration_min, user_name
+                ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
             if not forceplay:
                 db[chat_id] = []
-            await ZYRO.join_call(
+            await Hotty.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -185,7 +208,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
+            button = stream_markup(_, vidid, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
@@ -197,6 +220,7 @@ async def stream(
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
+
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "stream"
     elif streamtype == "soundcloud":
@@ -225,7 +249,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await ZYRO.join_call(chat_id, original_chat_id, file_path, video=None)
+            await Hotty.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -238,12 +262,12 @@ async def stream(
                 "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, chat_id)
+            button = stream_markup2(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
                 caption=_["stream_1"].format(
-                    config.SUPPORT_CHAT, title[:18], duration_min, user_name
+                    config.SUPPORT_CHAT, title[:23], duration_min, user_name
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
@@ -277,7 +301,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await ZYRO.join_call(chat_id, original_chat_id, file_path, video=status)
+            await Hotty.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -292,7 +316,7 @@ async def stream(
             )
             if video:
                 await add_active_video_chat(chat_id)
-            button = stream_markup(_, chat_id)
+            button = stream_markup2(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
@@ -324,7 +348,7 @@ async def stream(
             button = aq_markup(_, chat_id)
             await app.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
+                text=_["queue_4"].format(position, title[:18], duration_min, user_name),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
@@ -333,7 +357,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await ZYRO.join_call(
+            await Hotty.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -353,7 +377,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
+            button = stream_markup2(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
@@ -391,7 +415,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await ZYRO.join_call(
+            await Hotty.join_call(
                 chat_id,
                 original_chat_id,
                 link,
@@ -408,7 +432,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, chat_id)
+            button = stream_markup2(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.STREAM_IMG_URL,
@@ -418,3 +442,28 @@ async def stream(
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
             await mystic.delete()
+
+
+# Function to get thumbnail by video ID
+async def get_thumb(videoid):
+    try:
+        # Search for the video using video ID
+        query = f"https://www.youtube.com/watch?v={videoid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return config.YOUTUBE_IMG_URL
+
+
+async def get_thumb(vidid):
+    try:
+        # Search for the video using video ID
+        query = f"https://www.youtube.com/watch?v={vidid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return config.YOUTUBE_IMG_URL
