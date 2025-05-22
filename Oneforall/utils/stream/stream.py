2a1e387b4dfb695bbbfee3c35aa1f9760ab3027e
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2025-05-22 15:00:11 +0530

    Update stream.py

--- Oneforall/utils/stream/stream.py
+++ Oneforall/utils/stream/stream.py
@@ -11,7 +11,7 @@ from Oneforall.misc import db
 from Oneforall.utils.database import add_active_video_chat, is_active_chat
 from Oneforall.utils.exceptions import AssistantErr
 from Oneforall.utils.inline import aq_markup, close_markup, stream_markup
-from Oneforall.utils.pastebin import ZYROBin
+from Oneforall.utils.pastebin import HottyBin as ZYROBin
 from Oneforall.utils.stream.queue import put_queue, put_queue_index
 from Oneforall.utils.thumbnails import get_thumb
 
